1811c80a27e86a2cd329026b06252ad32812668c
Andy Gurevicz <agurevicz@frba.utn.edu.ar>
2025-09-17 15:20:57 -0300

    Router pedido enviado

--- packages/backend/routes/pedidoRoutes.js
+++ packages/backend/routes/pedidoRoutes.js
@@ -1,4 +1,4 @@
-import {PedidosController} from "../controllers/pedidosController.js"
+import { PedidosController } from "../controllers/pedidosController.js"
 import express from "express"
 import { generalErrorHandler } from "../middlewares/generalErrorHandler.js"
 import { pedidoErrorHandler } from "../middlewares/pedidoErrorHandler.js"
@@ -9,31 +9,42 @@ export default function pedidoRoutes(getController) {
 	const router = express.Router()
 
 	//Get pedidos
-	router.get(pathPedido, (req,res) => {
-	getController(PedidosController).buscarTodos(req,res)
+	router.get(pathPedido, (req, res) => {
+		getController(PedidosController).buscarTodos(req, res)
 	})
 	//Crear pedido
-	router.post(pathPedido,async (req,res,next) => {
+	router.post(pathPedido, async (req, res, next) => {
 		try {
-			await getController(PedidosController).crear(req,res)
-		} 
+			await getController(PedidosController).crear(req, res)
+		}
 		catch (error) {
 			next(error)
-	}})
-	
-     // Cancelar un pedido
-    router.post(`${pathPedido}/cancelar`, async (req, res, next) => {
-        try {
-            const { idPedido, motivo } = req.body; // asumimos que viene el id y motivo
-            await getController(PedidosController).cancelar(idPedido, motivo, req, res);
-        } catch (error) {
-            next(error);
-        }
-    });
+		}
+	})
+
+	// Cancelar un pedido
+	router.post(`${pathPedido}/cancelar`, async (req, res, next) => {
+		try {
+			const { idPedido } = req.body; // asumimos que viene el id
+			await getController(PedidosController).cancelar(idPedido, res);
+		} catch (error) {
+			next(error);
+		}
+	});
+
+	// Enviar un pedido
+	router.post(`${pathPedido}/enviar`, async (req, res, next) => {
+		try {
+			const { idPedido } = req.body;
+			await getController(PedidosController).enviar(idPedido, res);
+		} catch (error) {
+			next(error);
+		}
+	});
 
 	router.use(pedidoErrorHandler)
 	router.use(generalErrorHandler)
-	
+
 
 	return router
 }
