fae8d397fd3381de2c1a7da745ba4d566d981a1d
Yerilin <yvillarrealmamani@frba.utn.edu.ar>
2025-10-29 08:23:45 -0300

    agrego campos de filtro

--- packages/cliente/src/components/Filtros.jsx
+++ packages/cliente/src/components/Filtros.jsx
@@ -3,15 +3,15 @@ import { useState } from "react";
 import { useFiltro } from "../context/FiltroContext.jsx";
 
 const Filtro = ({ categorias = [] }) => {
+  //agregarmas vendido
+  const { selectedCategorias, toggleCategoria, clearCategorias } = useFiltro();
 
-    const { selectedCategorias, toggleCategoria, clearCategorias } = useFiltro();
-
-    const [filtros, setFiltros] = useState({
-		categoria: "",
-		precioMin: "",
-		precioMax: "",
-		orden: "asc",
-	});
+  const [filtros, setFiltros] = useState({
+    categoria: "",
+    precioMin: "",
+    precioMax: "",
+    orden: "asc",
+  });
 
   return (
     <div className="filtro-componente">
@@ -19,7 +19,7 @@ const Filtro = ({ categorias = [] }) => {
         <h3>Categorías</h3>
         <div className="filtro-grupo">
           {categorias.map((c) => (
-            <label key={c.id} className="check-categoria">
+            <label key={c.id} className="check-input">
               <input
                 type="checkbox"
                 value={String(c.id)}
@@ -29,7 +29,53 @@ const Filtro = ({ categorias = [] }) => {
               {c.nombre}
             </label>
           ))}
+        
+        {/* Rango de Precio */}
+        <div className="filtro-grupo">
+          <h3>Precio</h3>
+          <label>
+            Mínimo:
+            <input
+              type="number"
+              name="precioMin"
+            />
+          </label>
+          <label>
+            Máximo:
+            <input
+              type="number"
+              name="precioMax"
+              placeholder="1000"
+            />
+          </label>
         </div>
+        </div>
+        {/* Más Vendidos */}
+        <div className="filtro-grupo">
+          <h3>Más Vendidos</h3>
+          <label className="check-input">
+            <input 
+            type="checkbox" 
+            //onChange={()=>agregarMasVendido()}
+            />
+            Mas Vendidos
+          </label>
+        </div>
+
+        {/* Ordenamiento */}
+        <div className="filtro-grupo">
+          <h3>Ordenamiento</h3>
+          <label className="check-input">
+            <input type="checkbox" />
+            Precio ascendente
+          </label>
+          <label className="check-input">
+            <input type="checkbox" />
+            Precio descendente
+          </label>
+        </div>
+
+        
       </aside>
     </div>
   );
